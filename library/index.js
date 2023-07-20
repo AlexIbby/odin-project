296457fe20d768da93ce84fea137864400c64904
AlexIbby <alex.a.ibrahim@gmail.com>
2023-07-20 00:38:08 -0400

    Added sorting features and edit features for Odin Project library

--- library/index.js
+++ library/index.js
@@ -18,12 +18,21 @@ const addBookBtn = document.getElementById("add-book-btn")
 /*Add Book Form*/
 const form = document.querySelector('.add-book-form');
 
+/*Sort Library Buttons*/
+const sortTitleBtn = document.getElementById("sort-title")
+const sortAuthorBtn = document.getElementById("sort-author")
+const sortPagesBtn = document.getElementById("sort-pages")
+const deleteBtns = document.getElementsByClassName("delete-btn");
+
 
 /*********************************************
  *            Event Listners                 *
  *********************************************/
 
 addBookBtn.addEventListener("click", addBook);
+sortTitleBtn.addEventListener("click", sortTitle )
+sortAuthorBtn.addEventListener("click", sortAuthor)
+sortPagesBtn.addEventListener("click", sortPages)
 
 
 /*********************************************
@@ -39,7 +48,7 @@ function addBook(){
         addBookBtn.innerText = "Hide"
     } else {
         addBookContainer.classList.add('hide-form');
-        addBookBtn.innerText = "Add a Book"
+        addBookBtn.innerHTML = '<span id="plus-sign">+</span>Add a Book'
     }
     
 }
@@ -61,7 +70,8 @@ form.addEventListener('submit', async function(event) {
     let newBook = new Book(bookTitle, author, pages, read, cover)
 
     addToLocalStorage(newBook)
-    addBookCards()
+    addBookCards() 
+    
 });
 
 
@@ -116,6 +126,21 @@ function addToLocalStorage(book) {
     localStorage.setItem('books', JSON.stringify(storedBooks));
   }
 
+function getBooks() {
+    let books = localStorage.getItem('books');
+
+    if (books) {
+        return JSON.parse(books);
+    } else {
+        return [];
+    }
+}
+
+// Set the books to localStorage
+function setBooks(books) {
+    localStorage.setItem('books', JSON.stringify(books));
+}
+
 
 function showBooks(){
 
@@ -131,14 +156,22 @@ function addBookCards() {
 
     // Construct the cards HTML
     let cardsHtml = '';
-        for (const book of books) {
+    
+    for (const book of books) {
+        const readIconSrc = book.read === 'yes' ? '/images/read-yes.png' : '/images/read-no.png';
+        const readIconAlt = book.read === 'yes' ? 'Has been read' : 'Not read yet';
+
         cardsHtml += `
             <div class="book-card">
-                <img class="book-cover-card" src="${book.cover}" alt="${book.Title} Cover">
-                <p id="title-card">${book.bookTitle}</p>
+                <img class="book-cover-card" src="${book.cover}" alt="${book.bookTitle} Cover">
+                <p class="title-card">${book.bookTitle}</p>
                 <p class="author-card">${book.author}</p>
                 <p class="number-card">${book.pages} Pages</p>
-                <p>Read Yet?:${book.read} </p>
+                <p class="read-container">Read:  <img class="read-icon" src="${readIconSrc}" alt="${readIconAlt}"></p>
+                <div>
+                    <button class="delete-btn" >Remove</button>
+                    <button id="edit-btn" >Edit</button>
+                </div>
             </div>
         `;
     }
@@ -146,7 +179,281 @@ function addBookCards() {
     // Add the constructed HTML to the library div
     const libraryElement = document.querySelector('.library');
     libraryElement.innerHTML = cardsHtml;
+
+    // Add event listeners to all delete buttons after adding the cards
+    const deleteBtns = document.getElementsByClassName("delete-btn");
+    for (let btn of deleteBtns) {
+            btn.addEventListener("click", deleteBook);
+    }
 }
+
+
+function sortTitle(){
+    const storedBooks = localStorage.getItem('books');
+    const books = storedBooks ? JSON.parse(storedBooks) : [];
+
+    // Sorting the books by title
+    books.sort((a, b) => a.bookTitle.localeCompare(b.bookTitle));
+
+    let cardsHtml = '';
+
+    for (const book of books) {
+        const readIconSrc = book.read === 'yes' ? '/images/read-yes.png' : '/images/read-no.png';
+        const readIconAlt = book.read === 'yes' ? 'Has been read' : 'Not read yet';
+
+        cardsHtml += `
+            <div class="book-card">
+                <img class="book-cover-card" src="${book.cover}" alt="${book.bookTitle} Cover">
+                <p class="title-card">${book.bookTitle}</p>
+                <p class="author-card">${book.author}</p>
+                <p class="number-card">${book.pages} Pages</p>
+                <p class="read-container">Read:  <img class="read-icon" src="${readIconSrc}" alt="${readIconAlt}"></p>
+                <div>
+                    <button class="delete-btn" >Remove</button>
+                    <button id="edit-btn" >Edit</button>
+                </div>
+            </div>
+        `;
+    }
+
+
+    // Add the constructed HTML to the library div
+    const libraryElement = document.querySelector('.library');
+    libraryElement.innerHTML = cardsHtml;
+}
+
+function sortAuthor(){
+    const storedBooks = localStorage.getItem('books');
+    const books = storedBooks ? JSON.parse(storedBooks) : [];
+
+    // Sorting the books by title
+    books.sort((a, b) => a.author.localeCompare(b.author));
+
+    let cardsHtml = '';
+
+    for (const book of books) {
+        const readIconSrc = book.read === 'yes' ? '/images/read-yes.png' : '/images/read-no.png';
+        const readIconAlt = book.read === 'yes' ? 'Has been read' : 'Not read yet';
+
+        cardsHtml += `
+            <div class="book-card">
+                <img class="book-cover-card" src="${book.cover}" alt="${book.bookTitle} Cover">
+                <p class="title-card">${book.bookTitle}</p>
+                <p class="author-card">${book.author}</p>
+                <p class="number-card">${book.pages} Pages</p>
+                <p class="read-container">Read:  <img class="read-icon" src="${readIconSrc}" alt="${readIconAlt}"></p>
+                <div>
+                    <button class="delete-btn" >Remove</button>
+                    <button id="edit-btn" >Edit</button>
+                </div>
+            </div>
+        `;
+    }
+
+
+    // Add the constructed HTML to the library div
+    const libraryElement = document.querySelector('.library');
+    libraryElement.innerHTML = cardsHtml;
+}
+
+
+function sortPages() {
+    const storedBooks = localStorage.getItem('books');
+    const books = storedBooks ? JSON.parse(storedBooks) : [];
+
+    // Check current sorting order from localStorage
+    const currentSortOrder = localStorage.getItem('sortOrder') || "ascending";
+
+    if (currentSortOrder === "ascending") {
+        // If it's currently ascending, sort descending
+        books.sort((a, b) => parseInt(b.pages) - parseInt(a.pages));
+        localStorage.setItem('sortOrder', "descending");
+        sortPagesBtn.innerText = "No. of Pages (Ascending)"
+        
+    } else {
+        // If it's currently descending or not set, sort ascending
+        books.sort((a, b) => parseInt(a.pages) - parseInt(b.pages));
+        localStorage.setItem('sortOrder', "ascending");
+        sortPagesBtn.innerText = "No. of Pages (Descending)"
+    }
+
+    let cardsHtml = '';
+    for (const book of books) {
+        const readIconSrc = book.read === 'yes' ? '/images/read-yes.png' : '/images/read-no.png';
+        const readIconAlt = book.read === 'yes' ? 'Has been read' : 'Not read yet';
+
+        cardsHtml += `
+            <div class="book-card">
+                <img class="book-cover-card" src="${book.cover}" alt="${book.bookTitle} Cover">
+                <p class="title-card">${book.bookTitle}</p>
+                <p class="author-card">${book.author}</p>
+                <p class="number-card">${book.pages} Pages</p>
+                <p class="read-container">Read:  <img class="read-icon" src="${readIconSrc}" alt="${readIconAlt}"></p>
+                <div>
+                    <button class="delete-btn" >Remove</button>
+                    <button id="edit-btn" >Edit</button>
+                </div>
+            </div>
+        `;
+    }
+
+
+    // Add the constructed HTML to the library div
+    const libraryElement = document.querySelector('.library');
+    libraryElement.innerHTML = cardsHtml;
+}
+
+
+function deleteBook(event) {
+    const bookCard = event.target.closest(".book-card");
+
+    // Extract book details from the bookCard
+    const bookTitle = bookCard.querySelector(".title-card").textContent;
+    const author = bookCard.querySelector(".author-card").textContent;
+
+    // Store the book details in data attributes for later use
+    const confirmModal = document.getElementById("confirmModal");
+    confirmModal.setAttribute('data-book-title', bookTitle);
+    confirmModal.setAttribute('data-book-author', author);
+
+    // Show the confirmation modal
+    confirmModal.style.display = "block";
+}
+
+
+
+document.getElementById("confirmDelete").addEventListener("click", function() {
+    // Fetch the book details using the data attributes stored in the modal
+    const confirmModal = document.getElementById("confirmModal");
+    const bookTitle = confirmModal.getAttribute('data-book-title');
+    const author = confirmModal.getAttribute('data-book-author');
+
+    const storedBooks = JSON.parse(localStorage.getItem("books") || "[]");
+
+    const updatedBooks = storedBooks.filter(book => {
+        return book.bookTitle !== bookTitle || book.author !== author;
+    });
+
+    localStorage.setItem("books", JSON.stringify(updatedBooks));
+
+    // Find the book-card to delete based on the title. 
+    // We loop through all .title-card elements to find a match.
+    const titleCards = document.querySelectorAll(".title-card");
+    for (let titleCard of titleCards) {
+        if (titleCard.textContent === bookTitle) {
+            const bookCardToDelete = titleCard.closest(".book-card");
+            if (bookCardToDelete) {
+                bookCardToDelete.remove();
+                break;  // once found, no need to continue the loop
+            }
+        }
+    }
+
+    // Close the modal
+    confirmModal.style.display = "none";
+});
+
+document.getElementById("cancelDelete").addEventListener("click", function(){
+    const confirmModal = document.getElementById("confirmModal");
+    confirmModal.style.display = "none";
+})
+
+
+
+//Edit Modal Work
+// Function to open the modal
+function openModal(book) {
+    document.getElementById("edit-title").value = book.bookTitle;
+    document.getElementById("edit-author").value = book.author;
+    document.getElementById("edit-pages").value = book.pages + " Pages";
+    document.getElementById("edit-read").value = book.read.toLowerCase() === "yes" ? "yes" : "no";
+    document.getElementById("editModal").style.display = "block";
+}
+
+// Getting books from localStorage
+function getBooksFromStorage() {
+    let books = localStorage.getItem('books');
+    return books ? JSON.parse(books) : [];
+}
+
+// Event Listener for the Edit button
+document.addEventListener('click', function(e) {
+    if (e.target && e.target.id === 'edit-btn') {
+        // Get the book title from the respective card
+        let title = e.target.parentElement.parentElement.querySelector('.title-card').innerText;
+        let books = getBooksFromStorage();
+        let book = books.find(b => b.bookTitle === title);
+        
+        if (book) {
+            openModal(book);
+        }
+    }
+});
+
+// Function to close the modal
+function closeModal() {
+    document.getElementById("editModal").style.display = "none";
+}
+
+// Event Listener for the Cancel button
+document.getElementById("cancel-edit").addEventListener("click", function() {
+    closeModal();
+});
+
+function updateBookInStorage(bookTitle, updatedBook) {
+    // Get the current list of books from localStorage
+    let books = getBooksFromStorage();
+    
+    // Find the index of the book to be updated
+    let bookIndex = books.findIndex(b => b.bookTitle === bookTitle);
+
+    // If the book was found, update its details
+    if (bookIndex !== -1) {
+        // Save the current cover value
+        let currentCover = books[bookIndex].cover;
+        
+        // Set the cover value to the updatedBook
+        updatedBook.cover = currentCover;
+
+        // Now, update the book details in the array
+        books[bookIndex] = updatedBook;
+    }
+
+    // Save the updated list back to localStorage
+    localStorage.setItem('books', JSON.stringify(books));
+}
+
+
+document.getElementById("submit-edit").addEventListener("click", function() {
+    // Get the original book title
+    let originalTitle = document.getElementById("edit-title").value;
+
+    // Fetch updated values from the modal
+    
+    let updatedTitle = document.getElementById("edit-title").value;
+    let updatedAuthor = document.getElementById("edit-author").value;
+    let updatedPages = document.getElementById("edit-pages").value.replace(' Pages', ''); // Remove the " Pages" text
+    let updatedRead = document.getElementById("edit-read").value;
+
+    // Create an updated book object
+    let updatedBook = {
+        bookTitle: updatedTitle,
+        author: updatedAuthor,
+        pages: updatedPages,
+        read: updatedRead
+    };
+
+    // Update the book in localStorage
+    updateBookInStorage(originalTitle, updatedBook);
+
+    // Refresh or update the UI to reflect the changes (if you have a function for this)
+    addBookCards()
+
+    // Close the modal after updating
+    closeModal();
+});
+
+
 
 // Call the function on page load
 window.addEventListener('DOMContentLoaded', addBookCards);
